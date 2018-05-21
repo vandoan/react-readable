f68f5b241d96d4b87611653a71b0d4fb7feaa2b8
Van Doan <van.doan@mindbodyonline.com>
2018-05-20 20:14:01 -0700

    Readme - update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, compose } from 'redux'
+import thunk from 'redux-thunk'
+import './index.css'
+import App from './containers/App'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+	reducers,
+	{}, 
+	composeEnhancer(
+		applyMiddleware(thunk)
+	)
+)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>, 
+	document.getElementById('root')
+)
 registerServiceWorker();
